3af7e9ba2039effcee4ba0ca9564678d78685c08
Taam <contact.taamdev@gmail.com>
2025-10-23 17:00:39 +0700

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const socketIO = require('socket.io');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+// Lưu users online
+let onlineUsers = [];
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  // Khi user join
+  socket.on('user-join', (username) => {
+    const user = {
+      id: socket.id,
+      username: username
+    };
+    onlineUsers.push(user);
+    
+    // Gửi danh sách users cho tất cả
+    io.emit('update-users', onlineUsers);
+    
+    // Thông báo có người mới join
+    socket.broadcast.emit('user-joined', username);
+  });
+
+  // Khi gửi tin nhắn
+  socket.on('send-message', (data) => {
+    io.emit('receive-message', {
+      username: data.username,
+      message: data.message,
+      time: new Date().toLocaleTimeString()
+    });
+  });
+
+  // Khi user disconnect
+  socket.on('disconnect', () => {
+    onlineUsers = onlineUsers.filter(user => user.id !== socket.id);
+    io.emit('update-users', onlineUsers);
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
